b7d79790d669034727ea94502997640f1a2f98e9
Eko Muchamad Haryono <66320809+ekomh170@users.noreply.github.com>
2024-10-17 11:00:58 +0700

    inisiasi laravel untuk pertemuan ke-4 pake laravel 11

--- source_code_praktikum_belajar/data-types.php
+++
@@ -1,5 +0,0 @@
-<?php
-$nama = "Eko Muchamad Haryono";  # string
-$umur = 21;              # integer
-$ipk = 3.9;              # float
-$isMarried = false;       # boolean
--- source_code_praktikum_belajar/variable.php
+++
@@ -1,9 +0,0 @@
-<?php
-# membuat variable
-$nama = "Eko Muchamad Haryono";
-$jurusan = "Teknik Informatika";
-
-# memanggil variable
-echo $nama;
-echo "<br>";
-echo $jurusan;
