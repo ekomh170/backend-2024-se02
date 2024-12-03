d28f32108206c4c652818fa7fe844f04c3d8b65c
Eko Muchamad Haryono <66320809+ekomh170@users.noreply.github.com>
2024-12-03 21:55:30 +0700

    menghapus seluruh file laravel yg tidak di pake & menambahkan boilerpater & setup buat praktikum

---
+++ controller/fruitController.js
@@ -0,0 +1,56 @@
+/**
+ * TODO 3:
+ * - import fruits dari data/fruits.js
+ * - refactor variabel ke ES6 variable
+ */
+var fruits = "";
+
+/**
+ * TODO 4:
+ * - Buat method index.
+ * - Refactor function ke ES6 Arrow Function
+ * - Tampilkan data fruits.
+ *
+ * @hint - Gunakan looping for of
+ */
+function index() {}
+
+/**
+ * TODO 5:
+ * - Buat method store.
+ * - Refactor function ke ES6 Arrow Function
+ * - Menambahkan data baru ke array fruits.
+ *
+ * @param {string} name - Nama buah.
+ *
+ * @hint - Gunakan method push
+ */
+function store(name) {}
+
+/**
+ * TODO 6:
+ * - Buat method update.
+ * - Refactor function ke ES6 Arrow Function
+ * - Memperbarui data fruits.
+ *
+ * @param {number} position - Posisi atau index yang ingin diupdate.
+ * @param {string} name - Nama buah yang baru.
+ */
+function update(position, name) {}
+
+/**
+ * TODO 7:
+ * - Buat method destroy.
+ * - Refactor function ke ES6 Arrow Function
+ * - Menghapus data fruits.
+ *
+ * @param {number} position - Posisi atau index yang ingin dihapus
+ *
+ * @hint - Gunakan method splice
+ */
+function destroy(position) {}
+
+/**
+ * TODO 8: export method index, store, update, dan destroy
+ */
+module.exports = "";
---
+++ data/fruit.js
@@ -0,0 +1,9 @@
+/**
+ * TODO 1:
+ * - Buat array yang berisi data buah.
+ * - Refactor variable ke ES6 Variable.
+ */
+var fruits;
+
+// TODO 2: export variable fruits
+module.exports = "export kode di sini";
